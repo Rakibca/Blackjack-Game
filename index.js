5cb74b575b3867b4a6ee2971d7a02e867dd47ddb
Rakibul Islam <rakib.ca@gmail.com>
2025-05-23 20:44:44 -0600

    added random card function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 10;
-let secondCard = 4;
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,12 +10,21 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+// 1. Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+  return 5;
+}
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -29,9 +39,9 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 6;
+  // 3. Use the getRandomCard() to set the value of card
+  let card = getRandomCard();
   sum += card;
-  // Push the card to the cards array
   cards.push(card);
   console.log(cards);
   renderGame();
